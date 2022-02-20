e9a301087c16d607b07d643049de19143365ed96
Mauricio Campos <thoyferr@gmail.com>
2022-02-19 23:48:52 -0300

    finalizado

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -26,6 +26,43 @@ function cpu(numj){
     return reslt;
 }
 
+function ganador(ma, hu){ /* Aqui se verifica las jugadas */
+    if ((ma === "PIEDRA") && (hu === "PAPEL")){
+        gan = 1;
+        alert("Gana el jugador humano");
+    }
+    if ((ma === "PIEDRA") && (hu === "TIJERA")){
+        gan = 0;
+        alert("Gana la maquina");
+    }
+    if ((ma === "PAPEL") && (hu === "TIJERA")){
+        gan = 1;
+        alert("Gana el jugador humano");
+    }
+    if ((ma === "PAPEL") && (hu === "PIEDRA")){
+        gan = 0;
+        alert("Gana la maquina");
+    }
+    if ((ma === "TIJERA") && (hu === "PAPEL")){
+        gan = 0;
+        alert("Gana la maquina");
+    }
+    if ((ma === "TIJERA") && (hu === "PIEDRA")){
+        gan = 1;
+        alert("Gana el jugador humano");
+    }
+    if ((ma === "TIJERA") && (hu === "TIJERA")){
+        gan = 2;
+    }
+    if ((ma === "PAPEL") && (hu === "PAPEL")){
+        gan = 2;
+    }
+    if ((ma === "PIEDRA") && (hu === "PIEDRA")){
+        gan = 2;
+    }
+    return gan;
+}
+
 
 alert(`--Juego del Cachipum ---`);
 jugadas = prompt("Indique el numero de veces que jugara");
@@ -33,14 +70,19 @@ jugadas = parseInt(jugadas);
 
 for (i = 1; i <= jugadas; i++) {
 
-
     jugador = validar();
     jcpu = Math.trunc((Math.random()* 3) +1);
     jugCpu = cpu(jcpu);
-    
+    alert(`La maquina eligio ${jugCpu}`);
+    final = ganador(jugCpu, jugador);
+    if (final === 1){
+        alert(`"Felicitaciones usted a ganado la partida..."`);
 
-
-
+    }else if (final === 0){
+        alert(`¡Lo siento la maquina ha ganado esta partida!`);
+    }else{
+        alert(`WOW fue un empate contra la maquina`);
+    }
 
 }
 
